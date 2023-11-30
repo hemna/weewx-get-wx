09e6dc0e47d12db0a8190ea51e0d1478fe032fa3
Hemna <waboring@hemna.com>
2023-11-30 17:40:03 -0500

    Output json too

--- weewx_get_wx.py
+++ weewx_get_wx.py
@@ -9,10 +9,10 @@ from rich.console import Console
 
 import click
 
+CLIENT_ID = "weewx-fetch"
 TOPIC = "weather/loop"
-
-#OUTFILE = "/home/waboring/allsky/tmp/weewx-wx.txt"
-OUTFILE = "./weewx-wx.txt"
+OUTFILE_TXT = "./weewx-wx.txt"
+OUTFILE_JSON = "./weewx-wx.json"
 
 def on_connect(client, userdata, flags, rc):
     """The callback for when the client receives a CONNACK response from the server."""
@@ -32,19 +32,57 @@ def on_message(client, userdata, msg):
     #console.print_json(data=wx_data)
     dt = datetime.fromtimestamp(float(wx_data['dateTime']))
 
+    temperature = f"{float(wx_data['outTemp_F']):.1F}"
+    humidity = f"{float(wx_data['outHumidity']):.0F}"
+    dewpoint = f"{float(wx_data['dewpoint_F']):.1F}"
+    wind_speed = f"{float(wx_data['windSpeed_mph']):.1f}"
+    wind_dir = f"{float(wx_data.get('windDir', 0.00))}"
+    pressure = "{float(wx_data['relbarometer']):.2f}"
+
     wx_str = (
         f"{dt:%Y-%m-%d %H:%M:%S}  "
-        f"Temp: {float(wx_data['outTemp_F']):.1f}F  "
-        f"Humidity: {float(wx_data['outHumidity']):.0f}%  "
-        f"Dewpoint: {float(wx_data['dewpoint_F']):.1f}F  "
-        f"Wind: {float(wx_data['windSpeed_mph']):.1f}mph@{float(wx_data.get('windDir', 0.00))}  "
-        f"Pressure: {float(wx_data['relbarometer']):.2f}inHg"
+        f"Temp: {temperature}F  "
+        f"Humidity: {humidity}%  "
+        f"Dewpoint: {dewpoint}F  "
+        f"Wind: {wind_speed}mph@{wind_dir}  "
+        f"Pressure: {pressure}inHg"
     )
     console.print(wx_str)
 
+    wx_json = {
+        'WX_TEMP': {
+            "value": temperature,
+            "format": "{:.1f}",
+        } ,
+        'WX_HUMIDITY': {
+            "value": humidity,
+            "format": "{:.0f}",
+            },
+        'WX_DEWPOINT': {
+            "value": dewpoint,
+            "format": "{:.1f}",
+            },
+        'WX_WIND_SPEED': {
+            "value": wind_speed,
+            "format": "{:.1f}",
+            },
+        'WX_WIND_DIR': {
+            "value": wind_dir,
+            "format": "{:.0f}",
+            },
+        'WX_PRESSURE': {
+            "value": pressure,
+            "format": "{:.2f}",
+        }
+    }
+    console.print(wx_json)
+
     # Now write the temperature and dewpoint to a file
-    with open(OUTFILE, "w") as f:
+    with open(OUTFILE_TXT, "w") as f:
         f.write(wx_str)
+
+    with open(OUTFILE_JSON, "w") as f:
+        f.write(json.dumps(wx_json))
 
 
 def on_disconnect(client, userdata, rc):
@@ -68,7 +106,7 @@ def wx(host, port, username, password):
     console = Console()
 
     client = mqtt.Client(
-        client_id="allsky-fetch",
+        client_id=CLIENT_ID,
         transport='tcp',
         protocol=mqtt.MQTTv311,
     )
@@ -84,7 +122,6 @@ def wx(host, port, username, password):
     console.print(f"Connecting to {host}:{port}")
     client.connect(host, port, 60)
     console.print("Starting loop")
-    #client.loop(timeout=60, max_packets=10)
     client.loop_forever(timeout=60, max_packets=10)
 
 
